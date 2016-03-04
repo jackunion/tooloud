359b5640d18c7a270d86e7fb419827aacdf38b2b
Jack Union <verteletskydmitry@gmail.com>
2016-03-04 17:05:35 +0300

    updated dist/tooloud.js

--- dist/tooloud.js
+++ dist/tooloud.js
@@ -234,29 +234,40 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports) {
 
 	var Worley = (function() {
-	    var OFFSET_BASIS = 2166136261;
-	    var FNV_PRIME = 16777619;
+	    function xorshift(seed) {
+	        x = seed ^ (seed >> 12);
+	        x = x ^ (x << 25);
+	        x = x ^ (x >> 27);
+	        return x * 2;
+	    }
+
+	    function hash(i, j, k) {
+	        return (((((2166136261 ^ i) * 16777619) ^ j) * 16777619) ^ k) * 16777619 & 0xffffffff;
+	    }
+
+	    function d(p1, p2) {
+	        return [p1.x - p2.x, p1.y - p2.y, p1.z - p2.z];
+	    }
 
 	    function EuclideanDistance(p1, p2) {
-	        return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)
-	            + (p1.z - p2.z) * (p1.z - p2.z);
+	        return d(p1, p2).reduce(function(sum, x) { return sum + (x * x) }, 0);
 	    }
 
 	    function ManhattanDistance(p1, p2) {
-	        return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y) + Math.abs(p1.z - p2.z);
+	        return d(p1, p2).reduce(function(sum, x) { return sum + Math.abs(x) }, 0)
 	    }
 
 	    function probLookup(value) {
 	        value = value & 0xffffffff;
-	        if (value < 393325350) return 1 & 0xffffffff;
-	        if (value < 1022645910) return 2 & 0xffffffff;
-	        if (value < 1861739990) return 3 & 0xffffffff;
-	        if (value < 2700834071) return 4 & 0xffffffff;
-	        if (value < 3372109335) return 5 & 0xffffffff;
-	        if (value < 3819626178) return 6 & 0xffffffff;
-	        if (value < 4075350088) return 7 & 0xffffffff;
-	        if (value < 4203212043) return 8 & 0xffffffff;
-	        return 9 & 0xffffffff;
+	        if (value < 393325350) return 1;
+	        if (value < 1022645910) return 2;
+	        if (value < 1861739990) return 3;
+	        if (value < 2700834071) return 4;
+	        if (value < 3372109335) return 5;
+	        if (value < 3819626178) return 6;
+	        if (value < 4075350088) return 7;
+	        if (value < 4203212043) return 8;
+	        return 9;
 	    }
 
 	    function insert(arr, value) {
@@ -267,16 +278,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	            arr[i] = value;
 	            if (i + 1 < arr.length) arr[i + 1] = temp;
 	        }
-	    }
-
-	    function lcgRandom(lastValue) {
-	        return (((1103515245 & 0xffffffff) * lastValue + (12345 & 0xffffffff)) % 0x100000000) & 0xffffffff;
-	    }
-
-
-	    function hash(i, j, k) {
-	        return ((((((OFFSET_BASIS ^ (i & 0xffffffff)) * FNV_PRIME) ^ (j & 0xffffffff)) * FNV_PRIME)
-	            ^ (k & 0xffffffff)) * FNV_PRIME) & 0xffffffff;
 	    }
 
 	    function noise(input, distanceFunc) {
@@ -302,16 +303,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	                    cubeX = evalCubeX + i;
 	                    cubeY = evalCubeY + j;
 	                    cubeZ = evalCubeZ + k;
-	                    lastRandom = lcgRandom(hash((cubeX + Seed) & 0xffffffff, (cubeY) & 0xffffffff, (cubeZ) & 0xffffffff));
+	                    lastRandom = xorshift(hash((cubeX + Seed) & 0xffffffff, (cubeY) & 0xffffffff, (cubeZ) & 0xffffffff));
 	                    numberFeaturePoints = probLookup(lastRandom);
 	                    for (var l = 0; l < numberFeaturePoints; ++l) {
-	                        lastRandom = lcgRandom(lastRandom);
+	                        lastRandom = xorshift(lastRandom);
 	                        randomDiff.X = lastRandom / 0x100000000;
 
-	                        lastRandom = lcgRandom(lastRandom);
+	                        lastRandom = xorshift(lastRandom);
 	                        randomDiff.Y = lastRandom / 0x100000000;
 
-	                        lastRandom = lcgRandom(lastRandom);
+	                        lastRandom = xorshift(lastRandom);
 	                        randomDiff.Z = lastRandom / 0x100000000;
 
 	                        featurePoint
@@ -320,11 +321,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	                    }
 	                }
 
-	        var color = distanceArray[0];
-	        if (color < 0) color = 0;
-	        if (color > 1) color = 1;
-
-	        return color;
+	        return distanceArray.map(function(x) { return x < 0 ? 0 : x > 1 ? 1 : x });
 	    }
 
 	    function Euclidean(x, y, z, seed) {
