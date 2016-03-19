4c6d62f18145d3238a4dc07fe2f07b20e6724d00
Jack Union <verteletskydmitry@gmail.com>
2016-03-19 16:19:28 +0300

    implementation sources

--- dist/tooloud.js
+++ dist/tooloud.js
@@ -61,6 +61,12 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
+	/*
+	    Perlin noise implementation taken from http://mrl.nyu.edu/~perlin/noise/
+	    Simplex noise implementation taken from http://www.csee.umbc.edu/~olano/s2002c36/ch02.pdf
+	    Worley noise implementation taken from https://aftbit.com/cell-noise-2/
+	*/
+
 	var Perlin = __webpack_require__(2),
 	    Simplex = __webpack_require__(3),
 	    Worley = __webpack_require__(4),
