9240129203b45132859a29d95bcb089327efa3fb
jackunion <verteletskydmitry@gmail.com>
2018-10-04 21:17:53 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './index.js',
     mode: 'production',
     output: {
         filename: 'tooloud.min.js',
